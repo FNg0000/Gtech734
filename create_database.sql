af6182a4b57b5da1a40cd3fb00df5238d31c370b
Fred <FNg0000@users.noreply.github.com>
2017-03-15 19:10:23 -0400

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -10,3 +10,5 @@ CREATE DATABASE lidar_project
     LC_CTYPE = 'English_United States.1252'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
+
+-- Database backup file located here: https://drive.google.com/file/d/0B7Fxq2mWeyTyN1lRcUdzOWxaMnM/view?usp=sharing
